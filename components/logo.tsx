70f1fd8ce45a088ac49a9a7b291a6a9030a90ab8
JwaSangHeon <whk4051@naver.com>
2024-01-08 15:38:26 +0900

    logo component 생성

---
+++ components/logo.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+  src: "../public/fonts/font.ttf",
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div
+        className="
+        hover:opacity-75
+        transition
+        items-center
+        gap-x-2
+        hidden
+        md:flex
+      "
+      >
+        <Image src="/logo.svg" alt="logo" height={30} width={30} />
+        <p
+          className={cn("text-lg text-neutral-700 pt-1", headingFont.className)}
+        >
+          일처리
+        </p>
+      </div>
+    </Link>
+  );
+};
