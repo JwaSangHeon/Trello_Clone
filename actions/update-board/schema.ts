97edbf2505834a2e9239ad10b7f9525ba30c9470
JwaSangHeon <whk4051@naver.com>
2024-05-02 11:59:56 +0900

    board 네임 수정, board 삭제

---
+++ actions/update-board/schema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const UpdateBoard = z.object({
+  title: z
+    .string({
+      required_error: "제목은 필수값입니다.",
+      invalid_type_error: "제목은 필수값입니다.",
+    })
+    .min(3, {
+      message: "제목은 3글자 이상이여야합니다.",
+    }),
+  id: z.string(),
+});
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,5 +1,6 @@
 import { Board } from "@prisma/client";
 import BoardTitleForm from "./board-title-form";
+import BoardOptions from "./board-options";
 
 interface BoardNavbarProps {
   data: Board;
@@ -9,6 +10,9 @@ const BoardNavbar = ({ data }: BoardNavbarProps) => {
   return (
     <div className="w-full h-14 z-[40] flex items-center px-6 text-white gap-x-4 fixed top-14 bg-black/50">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
