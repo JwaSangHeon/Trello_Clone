d3d3539ad160c6dd4d0336bf521e460a82d71853
JwaSangHeon <whk4051@naver.com>
2024-06-03 16:40:31 +0900

    card 추가 기능

---
+++ actions/create-card/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const CreateCard = z.object({
+  title: z
+    .string({
+      required_error: "제목은 필수값입니다.",
+      invalid_type_error: "제목은 필수값입니다.",
+    })
+    .min(3, {
+      message: "제목은 3글자 이상이여야합니다.",
+    }),
+  boardId: z.string(),
+  listId: z.string(),
+});
